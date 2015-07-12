29d742e3c39d3f7d9e8800b552b6d92d0c1961ca
choeminjun <choeminjun@naver.com>
2015-07-12 16:46:57 +0900

    전채

---
+++ book/randomspirals.py
@@ -0,0 +1,20 @@
+author = "최민준"
+import random
+import turtle
+t = turtle.Pen()
+turtle.bgcolor("black")
+colors = ["red", "yellow", "blue", "green", "orange", "purple",
+          "while", "gray"]
+for n in range(50):
+    t.pencolor(random.choice(colors))
+    size = random.randint(10, 40)
+    x = random.randrange(-turtle.window_width()//2,
+                         turtle.window_width()//2)
+    y = random.randrange(-turtle.window_height()//2,
+                         turtle.window_height()//2)
+    t.penup()
+    t.setpos(x, y)
+    t.pendown()
+    for m in range(size):
+        t.forward(m * 2)
+        t.left(91)
