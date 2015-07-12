0a5ef3b556fc214f8ab96e27c2a0951aea81fa84
choeminjun <choeminjun@naver.com>
2015-07-12 17:18:29 +0900

    Update randomspirals.py

--- book/randomspirals.py
+++ book/randomspirals.py
@@ -1,16 +1,16 @@
 import random
 import turtle
 t = turtle.Pen()
-turtle.bgcolor("black")
+turtle.bgcolor("black")  #ask 경고3
 colors = ["red", "yellow", "blue", "green", "orange", "purple",
           "while", "gray"]
 for n in range(50):
     t.pencolor(random.choice(colors))
     size = random.randint(10, 40)
-    x = random.randrange(-turtle.window_width()//2,
-                         turtle.window_width()//2)
-    y = random.randrange(-turtle.window_height()//2,
-                         turtle.window_height()//2)
+    x = random.randrange(-turtle.window_width()//2,  #ask 경고1
+                         turtle.window_width()//2)   #ask 경고4
+    y = random.randrange(-turtle.window_height()//2,  #ask 경고2
+                         turtle.window_height()//2)    #ask 경고5
     t.penup()
     t.setpos(x, y)
     t.pendown()
