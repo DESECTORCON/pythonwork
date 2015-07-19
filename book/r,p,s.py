5a0a35c8b0d69598e586eb555dda3c410cc69cad
choeminjun <choeminjun@naver.com>
2015-07-19 10:08:18 +0900

    전채

--- book/r,p,s.py
+++ book/r,p,s.py
@@ -2,7 +2,7 @@ import random
 lele = 1
 computer2 = 0
 player2 = 0
-choices = ["주먹", "종이", "가위"]
+choices = ["주먹", "종이", "가위","주먹", "종이", "가위"]
 print("'가위 바위 보' 게임 첫번째판")
 print("주먹은 가위를 없에고,가위는 종이를 없에고,종이는 주먹을 없엔다.")
 player = input("너는 주먹이 되고싶니; 아니면 종이; 아니면 가위가 되고십니?(그만하고싶으면 'Q'):")
@@ -13,26 +13,26 @@ while player != "q":
     if player == computer:
         print("우리 서로 똑같은것을 낸내.")
     elif player == "주먹":
-        if computer == "기위:":
+        if computer == "가위":
             print("너가 이겼어.")
-            player2 = player2 + 1
+            player2 = player2 + 1  # 약한 애러
         else:
             print("네가 이겼어.")
-            computer2 = computer2 + 1
-    elif player == "종이:":
-        if computer == "주먹:":
+            computer2 = computer2 + 1  # 약한 애러
+    elif player == "종이":
+        if computer == "주먹":
             print("너가 이겼어.")
-            player2 = player2 + 1
+            player2 = player2 + 1   # 약한 애러
         else:
             print("네가 이겼어.")
-            computer2 = computer2 + 1
-    elif player == "가위:":
-        if computer == "종이:":
+            computer2 = computer2 + 1    # 약한 애러
+    elif player == "가위":
+        if computer == "종이":
             print("너가 이겼어.")
-            player2 = player2 + 1
+            player2 = player2 + 1    # 약한 애러
         else:
             print("네가 이겼어.")
-            computer2 = computer2 + 1
+            computer2 = computer2 + 1   # 약한 애러
     else:
         print("타이핑에 애러가 있었어.")
     lele = lele + 1
