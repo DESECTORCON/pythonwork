2fe282e02cd8cd9431d99ef96bbc05246c881558
choeminjun <choeminjun@naver.com>
2015-08-01 15:20:22 +0900

    전채
    애러 부분을 고쳐 주세요!!

--- book/캘러스코프 개발후.py
+++ book/캘러스코프 개발후.py
@@ -2,7 +2,7 @@ import random
 import turtle
 t = turtle.Pen()
 t.speed(0)
-turtle.bgcolor("black")
+turtle.bgcolor("black")  # 애러('bgcolor' 를 turtle 라이버리에서 찾을 수 없음.)
 colors = ["red", "yellow", "blue", "green", "orange", "purple", "white", "gray"]
 def fff():
     for m in range(size):
@@ -15,8 +15,8 @@ def ddd(dfd1, dfd2):
 for n in range(50):
     t.pencolor(random.choice(colors))
     size = random.randint(10, 40)
-    x = random.randrange(0, turtle.window_width()//2)
-    y = random.randrange(0, turtle.window_height()//2)
+    x = random.randrange(0, turtle.window_width()//2) # 문제1('window_width'를 turtle 라이버리에서 찾을 수 없음)
+    y = random.randrange(0, turtle.window_height()//2) # 문제('window height'를 turtle 라이버리에서 찾을 수 없음)
     ddd(x, y)
     fff()
     ddd(-x, y)
