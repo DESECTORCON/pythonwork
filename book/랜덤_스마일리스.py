8ecb834be7c5764572f76e7183aee0745d75c1c5
choeminjun <choeminjun@naver.com>
2015-08-02 14:58:49 +0900

    전채
    조금만 기다려주새요!!

---
+++ book/랜덤_스마일리스.py
@@ -0,0 +1,43 @@
+import random
+import turtle
+t = turtle.Pen()
+t.speed(-50)
+t.hideturtle()
+turtle.bgcolor("black")
+
+
+def draw_smiley(x, y):
+    t.penup()
+    t.setpos(x, y)
+    t.pendown()
+    # 머리
+    t.pencolor("yellow")
+    t.fillcolor("yellow")
+    t.begin_fill()
+    t.circle(50)
+    t.end_fill()
+    # 왼쪽눈
+    t.setpos(x-15, y+60)
+    t.fillcolor("blue")
+    t.begin_fill()
+    t.circle(10)
+    t.end_fill()
+    # 오른쪽눈
+    t.setpos(x+15, y+60)
+    t.begin_fill()
+    t.circle(10)
+    t.end_fill()
+    # 입
+    t.setpos(x-25, y+40)
+    t.pencolor("black")
+    t.width(10)
+    t.goto(x-10, y+20)
+    t.goto(x+10, y+20)
+    t.goto(x+25, y+40)
+    t.width(1)
+for n in range(50):
+    x = random.randrange(-turtle.window_width()//2,
+                         turtle.window_width()//2)
+    y = random.randrange(-turtle.window_height()//2,
+                         turtle.window_height()//2)
+    draw_smiley(x, y)
