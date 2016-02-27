f2c6a61b3744f17cceedc449a0028b01b15cb84a
choeminjun <choeminjun@naver.com>
2016-02-27 16:46:20 +0900

    update
    상관X

--- making_games_with_python&pygame/Slide_Puzzle.py
+++ making_games_with_python&pygame/Slide_Puzzle.py
@@ -22,6 +22,7 @@ GREEN =       (   0, 204,   0)
 
 BGCOLOR = DARKTURQUOISE
 TILECOLOR = GREEN
+TEXTCOLOR = WHITE
 BOARDERCOLOR = BRIGHTBLUE
 BASICFONTSIZE = 30
 
@@ -44,3 +45,115 @@ def main():
     global FPSCOLOCK, DISPLATSURF, BASICFONT, RESET_SURF, REST_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT
 
     pygame.init()
+    FPSCOLOCK = pygame.time.Clock()
+    DISPLATSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('Slide Puzzle')
+    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+
+
+    RESET_SURF, REST_RECT = makeText('Reset', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 90)
+    NEW_SURF, NEW_RECT = makeText('New Game', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
+    SOLVE_SURF, SOLVE_RECT = makeText('Solve', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 30)
+
+    mainBoard, solutionSeq = generatNewPuzzle(80)
+    SOLVEDBOARD = getStartingBoard()
+    allMoves = []
+
+    while True:
+        slideTo = None
+        msg = ''
+        if mainBoard == SOLVEDBOARD:
+            msg = 'Solved!'
+
+        drawboard(mainBoard, msg)
+
+        checkForQuit()
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONUP:
+                spotx, spoty = getSpotClocked(mainBoard, event.pos[0], event.pos[1])
+
+                if (spotx, spoty) == (None, None):
+                    if REST_RECT.collidepoint(event.pos):
+                        resetAnimation(mainBoard, allMoves)
+
+                        allMoves = []
+                    elif NEW_RECT.collidepoint(event.pos):
+                        mainBoard, solutionSeq = generateNewPuzzle(80)
+
+                        allMoves = []
+                    elif SOLVE_RECT.collidepoint(event.pos):
+                        resetAnimation(mainBoard, solutionSeq + allMoves)
+
+                        allMoves = []
+                else:
+
+                    blankx, blanky = getBlankPosition(mainBoard)
+                    if spotx == blankx + 1 and spoty == blanky:
+                        slideTo = LEFT
+                    elif spotx == blankx - 1 and spoty == blanky:
+                        slideTo = RIGHT
+                    elif spotx == blankx and spoty == blanky + 1:
+                        slideTo = UP
+                    elif spotx == blankx and spoty == blanky - 1:
+                        slideTo = DOWN
+            elif event.type == KEYUP:
+
+                if event.key in (K_LEFT, K_a) and isValidMove(mainBoard, LEFT):
+                    slideTo = LEFT
+                elif event.key in (K_RIGHT, K_d) and isValidMove(mainBoard, RIGHT):
+                    slideTo = RIGHT
+                elif event.key in (K_UP, K_w) and isValidMove(mainBoard, UP):
+                    slideTo = UP
+                elif event.key in (K_DOWN, K_w) and isValidMove(mainBoard, DOWN):
+                    slideTo = DOWN
+
+        if slideTo:
+            slideAnimation(mainBoard, slideTo, 'Click tile or press arrow keys to slide.', 8)
+            makeMove(mainBoard, slideTo)
+            allMoves.append(slideTo)
+        pygame.display.update()
+        FPSCOLOCK.tick(FPS)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def checkForQuit():
+    for event in pygame.event.get(QUIT):
+        terminate()
+    for event in pygame.event.get(KEYUP):
+        if event.key == K_ESCAPE:
+            terminate()
+        pygame.event.post(event)
+
+
+def getStartingBoard():
+
+
+    counter = 1
+    board = []
+    for x in range(BOARDWIDTH):
+        column = []
+        for y in range(BOARDHEIGHT):
+            column.append(counter)
+            counter += BOARDWIDTH
+        board.append(column)
+        counter -= BOARDWIDTH * (BOARDHEIGHT - 1) + BOARDWIDTH - 1
+
+    board[BOARDWIDTH-1][BOARDHEIGHT-1] = None
+    return board
+
+
+def getBlankPosition(board):
+
+    for x in range(BOARDWIDTH):
+        for y in range(BOARDHEIGHT):
+            if board[x][y] == None:
+                return (x, y)
+
+
+def makeMove(board, move):
+
+    # 만드는중 줄:159
