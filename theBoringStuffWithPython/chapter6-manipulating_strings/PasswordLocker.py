2312d1ac9f7d011f33d585e568d5fb6b72365356
choeminjun <choeminjun@naver.com>
2016-04-16 16:35:02 +0900

    update

---
+++ theBoringStuffWithPython/chapter6-manipulating_strings/PasswordLocker.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3.4
+# pw.py - An insecure password locker program.
+
+PASSWORDS = {'email': 'tyuri847yuek674YHE734uyeh',
+             'blog': 'gdhju5yre647YEH4yrhe',
+             'luggage': '123',
+             'YL':'1234567890'}
+print(PASSWORDS)
