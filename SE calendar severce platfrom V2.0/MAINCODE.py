1f804c6d5fb6b479ba132e4f007008e23fd56623
choeminjun <choeminjun@naver.com>
2016-07-16 15:31:47 +0900

    Update
    설명update

--- SE calendar severce platfrom V2.0/MAINCODE.py
+++ SE calendar severce platfrom V2.0/MAINCODE.py
@@ -10,13 +10,15 @@ import datetime, time, calendar                            # 이 프로그램에
 import alarmSE, stopwachSE, sysSE, timeprinterSE, calendarManagerSE, timeprinterSE
 
 
-print('----------------calendar----------------')
+print('----------------calendar----------------')   # 시작화면 준비
 print('     welcome to s.e severce platfrom!    ')
 while True:
     print('Do you want to check the time or set alarm or see the clander?') # 사용자의 명령을 'USRN'
     USRIN = input()                                                         # 변수에 저장.
 
 
+    # 만약 USRIN의 값이 'time'면 날짜를 같이 볼 것인지 물어보고 아니면 timeprinterSE.timeprint.timewithoutdate()
+    # 을 통해 날짜를 같이 프린트하고 맞으면 timeprinterSE.timeprint.timewithdate()로 통해 날짜도 같이 프린트한다.
     if USRIN == 'time':
         while True:
 
@@ -35,16 +37,20 @@ while True:
             print(DATEV)
 
 
+    # 만약 USRIN의 값이 'clander'이면 calendarManagerSE.clanderM.clander()을 통해 달력을 화면에 표시한다.
     elif USRIN == 'clander':
         calendarManagerSE.clanderM.clander()
 
 
+    # 만약 USRIN의 값이 'alarm'이면 alarmSE.alarm()를 통해 알람을 실행한다.
     elif USRIN == 'alarm':
         alarmSE.alarm()
 
+    # 만약 USRIN의 값이 'stopwach' 이면 stopwachSE.stopwatch()를 통해 스톱워치을 실행한다.
     elif USRIN == 'stopwach':
         stopwachSE.stopwatch()
 
+    # 전원 끄기
     elif USRIN == 'OFF':
         sysSE.OFF()
 
